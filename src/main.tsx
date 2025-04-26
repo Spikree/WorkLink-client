4915cdefac9c2aebfd74d584209f294f542e94a2
spikree <avimahalingpure10@gmail.com>
2025-04-26 14:59:02 +0530

    Add react-error-boundary for error handling in main component

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorFallBack from "./error/ErrorFallBack.tsx";
 import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <ErrorBoundary FallbackComponent={ErrorFallBack} onReset={() => window.location.reload()}>
+        <App />
+      </ErrorBoundary>
     </BrowserRouter>
   </StrictMode>
 );
